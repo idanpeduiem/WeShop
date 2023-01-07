cb9bcbda0fbed7f7d83c072d1a59f79931b3f5b0
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2023-01-07 17:15:49 +0200

    add pages with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./components/Home";
+import Profile from "./components/Profile";
+import ProductDetails from "./components/ProductDetails";
+import Cart from "./components/Cart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export enum Routes {
+  HOME = "/",
+  CART = "/cart",
+  PROFILE = "/profile",
+  PRODUCT_DETAILS = "/product-details",
 }
+
+const router = createBrowserRouter([
+  {
+    path: Routes.HOME,
+    element: <HomePage />,
+  },
+  {
+    path: Routes.CART,
+    element: <Cart />,
+  },
+  {
+    path: Routes.PROFILE,
+    element: <Profile />,
+  },
+  {
+    path: Routes.PRODUCT_DETAILS,
+    element: <ProductDetails />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,12 @@
+import { Routes } from "../App";
+
+const Profile = () => {
+  return (
+    <>
+      <h1>Profile Page</h1>
+      <a href={Routes.HOME}></a>
+    </>
+  );
+};
+
+export default Profile;
