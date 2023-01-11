c66cf00ec906e411dc355d0300e4d83a634c2f13
Nikole <nikole.waintraub@gmail.com>
2023-01-11 20:09:24 +0200

    add mongoose

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,26 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('WeShop awsome server');
+const {PORT, DB_CONNECTION_STR = ''} = process.env;
+
+mongoose.connect(DB_CONNECTION_STR);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+app.get('/',async (req: Request, res: Response) => {
+  res.send('WeShop awesome server');
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
