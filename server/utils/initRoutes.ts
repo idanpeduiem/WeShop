895547c8b334466d4f51e42e529e6f0bb3d870ba
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2023-01-18 19:14:08 +0200

    create routes enum

--- server/utils/initRoutes.ts
+++ server/utils/initRoutes.ts
@@ -2,8 +2,13 @@ import {Express} from "express";
 import {mongoDbManager} from "./mongoDbManager";
 import User from "../models/user";
 
+// Add here routes to make all routes uniq
+enum routes {
+    ALL_USERS = '/users',
+}
+
 const initRoutes = (app: Express) => {
-    app.get('/users', async ({res}) => {
+    app.get(routes.ALL_USERS, async ({res}) => {
         const data = await mongoDbManager.getAll(User);
         res?.send(data);
     })
