caea2663813da649b34be32cc9b739500a595166
Nikole <nikole.waintraub@gmail.com>
2023-01-26 20:59:36 +0200

    add request userid field

--- server/utils/types.ts
+++ server/utils/types.ts
@@ -2,3 +2,13 @@ import mongoose, { MongooseOptions } from "mongoose";
 
 export const ObjectID = mongoose.Schema.Types.ObjectId;
 export const UUID = mongoose.Schema.Types.UUID;
+
+
+
+declare global {
+  namespace Express {
+    export interface Request {
+      userId?: string
+    }
+  }
+}
