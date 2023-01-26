769f767be946bcde3337d25247c5f96d3f77764f
Nikole <nikole.waintraub@gmail.com>
2023-01-26 14:29:01 +0200

    finish page

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router";
 import { useQuery } from 'react-query'
-import { Button, FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup } from "@mui/material";
+import { Alert, Button, FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, Snackbar } from "@mui/material";
 import { getItemQuery } from "../queries";
 import { ItemDetails, ItemStock, Size } from "../utils/types";
 import React, { useState } from "react";
@@ -20,12 +20,14 @@ const ItemSizes: React.FC<ItemSizesProps> = ({itemStocks: sizeStocks, onSelectIt
          onChange={(event,value) => onSelectItem}
       >
         {
-          sizeStocks.map(sizeStock =>
+          sizeStocks.map(sizeStock => {
+            return (
            <FormControlLabel 
            disabled={sizeStock.quantity===0}
            value={sizeStock.size[0]._id} 
            control={<Radio />} 
-           label={sizeStock.size[0].description} />)
+           onClick={(value: any) => onSelectItem(value.target.value)}
+           label={sizeStock.size[0].description} />)})
         }
       </RadioGroup>
     </FormControl>
@@ -35,11 +37,39 @@ const ProductDetails:React.FC = () => {
   const { id = " "} = useParams();
   const [selectedSize, setSelectedSize] = useState<Size['_id']>();
   const [quantity, setQuantity] = useState(1);
+  const [isSizeError, setIsSizeError] = useState(false);
   const {data: item ,isLoading, isError} = useQuery<ItemDetails>(['item',id], () => getItemQuery(id));
 
-  const onSizeSelect = (itemId: string) => {
-    setSelectedSize(itemId);
+  const onSizeSelect = (sizeId: string) => {
+    setSelectedSize(sizeId);
+    console.log(sizeId);
   }
+
+  const onAddToCart = () => {
+    if(!selectedSize)
+     setIsSizeError(true);
+  }
+
+  const onAddToWishList = () => {
+
+  }
+
+  const onChangeQuantity = (isIncrement: boolean) => {
+    if(isIncrement){
+      setQuantity(prev => prev + 1);
+    }else {
+      if(quantity >= 2)
+      setQuantity(prev => prev - 1);
+     }
+  }
+
+  const QuantityBox = () => (
+    <div style={{display:'flex', flexDirection:'row'}}>
+    <Button disabled={quantity === 1}  onClick={() => onChangeQuantity(false)} >-</Button>
+    <p>{quantity}</p>
+    <Button onClick={() => onChangeQuantity(true)}>+</Button>
+   </div>
+  )
 
   if (isLoading) {
     return <span>Loading...</span>
@@ -50,7 +80,6 @@ const ProductDetails:React.FC = () => {
   }
 
   const {description,price,image,department,category,stock} = item;
-  console.log(item)
   return (
     <>
       <Grid container>
@@ -58,12 +87,30 @@ const ProductDetails:React.FC = () => {
           <h1>{description}</h1>
           <h2>{category.description} - {department.description}</h2>
           <h1>{price}₪</h1>
+          <div>
           <ItemSizes itemStocks={stock} onSelectItem={onSizeSelect}/>
-          <Button>Add to cart</Button>
-          <Button>Add to ♥</Button>
+          </div>
+          <div>
+          <QuantityBox/>
+          </div>
+          <div style={{marginTop: '30px'}}>
+            <Button variant="outlined"  onClick={onAddToCart}>Add to cart</Button>
+            <Button variant="outlined"  onClick={onAddToWishList}>Add to ♥</Button>
+          </div>
         </Grid>
         <Grid item xs={6} justifyItems='flex-end'>
           <img style={{height: "60vh", width:"60vh"}} alt ='' src={image}/>
+        </Grid>
+        <Grid item xs={12}>
+        <Snackbar
+        open={isSizeError}
+        onClose={() => setIsSizeError(false)}
+        autoHideDuration={3000}
+      >
+         <Alert onClose={() => setIsSizeError(false)} severity="error" sx={{ width: '100%' }}>
+           Please select size
+        </Alert>
+      </Snackbar>
         </Grid>
       </Grid>
     </>
