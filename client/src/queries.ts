507ce900e5726e830de25f3363dd6e06d6f2b005
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2023-02-01 16:37:40 +0200

    fix crashing when server is down

--- client/src/queries.ts
+++ client/src/queries.ts
@@ -2,19 +2,19 @@ import AxiosInstance from "./utils/axiosInstance";
 import { ItemDetails } from "./utils/types";
 import { User } from "firebase/auth";
 import { CartItem } from "./controller/cartController/cartProvider";
-import {wishlistItem} from "./controller/wishlistController/wishlistProvider";
+import { wishlistItem } from "./controller/wishlistController/wishlistProvider";
 
 // items
 
 export const getAllItems = async () =>
   await AxiosInstance.get("/items")
     .then((itemsRes) => itemsRes.data)
-    .catch(() => new Error(`Failed getting all products`));
+    .catch(() => []);
 
 export const getAllItemsDesc = async () =>
   await AxiosInstance.get(`/items/desc`)
     .then((itemsRes) => itemsRes.data)
-    .catch(() => new Error(`Failed getting all products description`));
+    .catch(() => []);
 
 export const getItemQuery = async (id: string): Promise<ItemDetails> =>
   await AxiosInstance.get(`/items/${id}`)
@@ -26,7 +26,7 @@ export const getItemQuery = async (id: string): Promise<ItemDetails> =>
 export const getItemsFromCart = (userId: User["uid"]): Promise<ItemDetails[]> =>
   AxiosInstance.get(`/carts/items/${userId}`)
     .then((itemData) => itemData.data)
-    .catch(() => new Error("something went wrong"));
+    .catch(() => []);
 
 export const addItemToCart = async (cartItem: CartItem) =>
   await AxiosInstance.post(`/carts/addItem`, { ...cartItem }).catch(
@@ -35,34 +35,32 @@ export const addItemToCart = async (cartItem: CartItem) =>
 
 // wishlist
 
-export const getItemsFromWishlist = (userId: User["uid"]): Promise<ItemDetails[]> =>
-    AxiosInstance.get(`/wishlist/items/${userId}`)
-        .then((itemData) => itemData.data)
-        .catch(() => new Error("something went wrong"));
+export const getItemsFromWishlist = (
+  userId: User["uid"]
+): Promise<ItemDetails[]> =>
+  AxiosInstance.get(`/wishlist/items/${userId}`)
+    .then((itemData) => itemData.data)
+    .catch(() => []);
 
-
-export const addItemToWishlist = async (wishlistItem: wishlistItem) =>
-    await AxiosInstance.post(`/wishlist/addItem`, { ...wishlistItem }).catch(
-        () => new Error("something went wrong")
-    );
+export const addItemToWishlist = (wishlistItem: wishlistItem) =>
+  AxiosInstance.post(`/wishlist/addItem`, { ...wishlistItem }).catch(() => []);
 
 // departments
 
 export const getAllDepartments = async () =>
   await AxiosInstance.get("/departments")
     .then((departmentsRes) => departmentsRes.data)
-    .catch(() => new Error(`Failed getting all departments`));
+    .catch(() => []);
 
 // categories
 
 export const getAllCategories = async () =>
   await AxiosInstance.get("/categories")
     .then((categoriesRes) => categoriesRes.data)
-    .catch(() => new Error(`Failed getting all categories`));
-
+    .catch(() => []);
 
 // orders
-  export const getAllUserOrders = async () =>
-   await AxiosInstance.get("/orders")
+export const getAllUserOrders = async () =>
+  await AxiosInstance.get("/orders")
     .then((ordersRes) => ordersRes.data)
-    .catch(() => new Error(`Failed getting all user orders`));
+    .catch(() => []);
