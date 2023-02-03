35e6c5618205c9dbd55ea0e3b94b5675fefc8657
Nikole <nikole.waintraub@gmail.com>
2023-02-03 19:14:06 +0200

    cart value to context

--- client/src/controller/cartController/cartContext.tsx
+++ client/src/controller/cartController/cartContext.tsx
@@ -3,12 +3,14 @@ import {CartItem, Item, ItemDetails } from "../../utils/types";
 
 interface CartContextProps {
   cartItems: CartItem[];
+  cartValue: number,
   addItem: (cartItem: CartItem) => void;
   removeItem: (id: Item["_id"]) => void;
 }
 
 const defaultValue: CartContextProps = {
   cartItems: [],
+  cartValue: 0,
   addItem: () => {},
   removeItem: () => {},
 };
