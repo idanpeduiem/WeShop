edba762be2f37a84dabacc94b1988e5ec1e1a519
DanaBg <danusha115@gmail.com>
2023-02-11 16:36:58 +0200

    add scrapingg

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import initRoutes from "./utils/initRoutes";
 import { validateToken } from "./utils/middlewares";
 import setUpMongo from './utils/mongoDbManager';
 import bodyParser from "body-parser";
+import { performScraping } from "./utils/scraping";
 
 const admin = require("firebase-admin");
 dotenv.config();
@@ -35,6 +36,8 @@ app.get("/", validateToken, (req: Request, res: Response) => {
 });
 
 setUpMongo();
+
+performScraping();
 
 app.use(validateToken);
 initRoutes(app);
