f991c9409aed52e1770f8751ce0136d1f334221a
DanaBg <danusha115@gmail.com>
2023-02-12 22:54:07 +0200

    delete unnecessary imports across app

--- client/src/components/common/ItemCard.tsx
+++ client/src/components/common/ItemCard.tsx
@@ -1,23 +1,15 @@
 import * as React from "react";
 import { useNavigate } from "react-router";
 import {
-  Badge,
   Card,
   CardActions,
   CardContent,
   CardMedia,
   Grid,
-  IconButton,
   Typography,
 } from "@mui/material";
-import { useCartContext } from "../../controller/cartController/cartContext";
 import { ItemDetails, Size } from "../../utils/types";
-import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
 import { RoutePaths } from "../../App";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import { useWishlistContext } from "../../controller/wishlistController/wishlistContext";
-import { useMemo, useState } from "react";
 import WishlistIcon from "./wishlistIcon";
 
 interface ItemCardProps {
